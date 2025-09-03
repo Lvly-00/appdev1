e7d62ebe7bb183d5599cb134736e9a418bc84d9c
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-09-03 10:15:13 +0800

    Add variables activity

---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,25 @@
+// Variables
+let name = "Alsathia";
+let age = 21;
+let isStudent = true;
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isStudent);
+
+// Arithmetic 
+let x = 10;
+let y = 5;
+
+let sum = x + y;
+console.log("Sum: " + sum);
+let difference = x - y;
+console.log("Difference: " + difference);
+let product = x * y;    
+console.log("Product: " + product);
+let quotient = x / y;   
+console.log("Quotient: " + quotient);
+
+// Comparison 
+console.log("5" == 5);
+console.log("5" === 5);
