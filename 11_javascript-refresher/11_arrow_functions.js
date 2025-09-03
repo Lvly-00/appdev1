5e95f2486c8791dedb3814c78a945780adfad637
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-09-03 15:49:38 +0800

    Add arrow functions activity

---
+++ 11_javascript-refresher/11_arrow_functions.js
@@ -0,0 +1,11 @@
+// No param
+const greet = () => console.log("Hello!");
+greet();
+
+// Single param
+const square = (num) => num * num;
+console.log("Square:", square(5));
+
+// Implicit return
+const sayHi = (name) => `Hi, ${name}!`;
+console.log(sayHi("Alsathia"));
