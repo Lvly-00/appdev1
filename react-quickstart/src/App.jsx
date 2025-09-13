a2d6b87d2b987c195bff6b512f91dc5ed63d2b6e
LPintes03 <lovelypintes@gmail.com>
2025-09-13 14:51:57 +0800

    Done displaying data

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -3,9 +3,17 @@ import './App.css'
 
 
 function App() {
+
+  const user = {
+    name: "Brainrot Cat",
+    imageURL: 'https://i.redd.it/a-side-effect-of-lackadaisy-brainrot-no-one-tells-you-is-v0-4vs0wh5f8d0c1.jpg?width=564&format=pjpg&auto=webp&s=c81c92b5e9fb45418839c5536042796d8200c386',
+    information: "This is a cat with brainrot",
+  }
   return (
     <>
-      <img className='avatar' src="https://i.redd.it/a-side-effect-of-lackadaisy-brainrot-no-one-tells-you-is-v0-4vs0wh5f8d0c1.jpg?width=564&format=pjpg&auto=webp&s=c81c92b5e9fb45418839c5536042796d8200c386" />
+      <img className='avatar' src={user.imageURL} />
+      <h1> {user.name}</h1>
+      <p> {user.information}</p>
     </>
   )
 }
