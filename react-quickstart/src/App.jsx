0a6de6af9cf0281b6fcc70ce98457ec84acae283
LPintes03 <lovelypintes@gmail.com>
2025-09-13 14:46:54 +0800

    Done adding styles

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,14 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 
-function AboutPage() {
-  return <> <h1>About</h1>
-    <p>Hello there, <br /> How do you do?</p></>
-}
+
 function App() {
   return (
     <>
-      <AboutPage />
+      <img className='avatar' src="https://i.redd.it/a-side-effect-of-lackadaisy-brainrot-no-one-tells-you-is-v0-4vs0wh5f8d0c1.jpg?width=564&format=pjpg&auto=webp&s=c81c92b5e9fb45418839c5536042796d8200c386" />
     </>
   )
 }
