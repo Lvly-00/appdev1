8dc60dfb52e6bdb2ded8f9f826f924d54d9afc9e
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-09-14 14:37:12 +0800

    Done conditional rendering

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,21 +1,48 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-
-function App() {
-
+function UserPanel() {
   const user = {
     name: "Brainrot Cat",
-    imageURL: 'https://i.redd.it/a-side-effect-of-lackadaisy-brainrot-no-one-tells-you-is-v0-4vs0wh5f8d0c1.jpg?width=564&format=pjpg&auto=webp&s=c81c92b5e9fb45418839c5536042796d8200c386',
+    imageURL:
+      "https://i.redd.it/a-side-effect-of-lackadaisy-brainrot-no-one-tells-you-is-v0-4vs0wh5f8d0c1.jpg?width=564&format=pjpg&auto=webp&s=c81c92b5e9fb45418839c5536042796d8200c386",
     information: "This is a cat with brainrot",
-  }
+  };
   return (
     <>
-      <img className='avatar' src={user.imageURL} />
+      <img className="avatar" src={user.imageURL} />
       <h1> {user.name}</h1>
       <p> {user.information}</p>
     </>
-  )
+  );
 }
 
-export default App
+function AdminPanel() {
+  const user = {
+    name: "Doggo",
+    imageURL:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNQHrzBBdeaMeZxt-kX_Mj2WDM8u0YkzlikQ&s",
+    information: "This Doggo is Admin ",
+  };
+  return (
+    <>
+      <img className="avatar" src={user.imageURL} />
+      <h1> {user.name}</h1>
+      <p> {user.information}</p>
+    </>
+  );
+}
+
+let user;
+
+if (false) {
+  user = <UserPanel />;
+} else {
+  user = <AdminPanel />;
+}
+
+function App() {
+  return <>{user}</>;
+}
+
+export default App;
