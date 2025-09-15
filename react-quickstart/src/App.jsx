e1f5734658442bde3e9dec6b240ab7f978a16f67
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-09-15 13:47:03 +0800

    Done responding to events / using hooks

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import { useState } from "react";
 import "./App.css";
 
-const elements = [
-  { element: "Anemo", id: 1 },
-  { element: "Geo", id: 2 },
-  { element: "Electro", id: 3 },
-  { element: "Dendro", id: 4 },
-  { element: "Pyro", id: 5 },
-  { element: "Cryo", id: 6 },
-];
-
 function App() {
-  const listElements = elements.map((elements) => (
-    <li key={elements.id}>{elements.element}</li>
-  ));
+  function ButtonCounter() {
+    const [count, setCount] = useState(0);
+    function handleCounting() {
+      setCount(count + 1);
+    }
+
+    return <button onClick={handleCounting}> {count} </button>;
+  }
+
   return (
     <>
-      <ul>{listElements}</ul>
+      <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+        <ButtonCounter />
+        <p>+</p>
+        <ButtonCounter />
+        <p>=</p>
+        <ButtonCounter />
+      </div>
     </>
   );
 }
