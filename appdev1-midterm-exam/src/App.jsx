cbc74cb088c2eb18af73492b13279930654a9480
LPintes03 <128210631+Lvly-00@users.noreply.github.com>
2025-10-04 13:02:08 +0800

    Revert "Removed SignInModal"
    This reverts commit 9aa929869fd52c650bfa9a113ef5c3a088d67713.

--- appdev1-midterm-exam/src/App.jsx
+++ appdev1-midterm-exam/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import Logo from "./assets/images/logo.png";
 import Slider from "./assets/images/slider-dec.png";
@@ -11,8 +11,6 @@ import WhiteLogo from "./assets/images/white-logo.png";
 import SignInModal from "./components/SignInModal";
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <>
       {/* ***** Preloader Start ***** */}
@@ -64,11 +62,9 @@ function App() {
                   <li>
                     <div className="gradient-button">
                       <a
-                        href="#"
-                        onClick={(e) => {
-                          e.preventDefault(); // prevent page jump
-                          setIsOpen(true);
-                        }}
+                        id="modal_trigger"
+                        onClick={() => setIsOpen(true)}
+                        href="#modal"
                       >
                         <i className="fa fa-sign-in-alt" /> Sign In Now
                       </a>
@@ -85,14 +81,110 @@ function App() {
         </div>
       </header>
       {/* ***** Header Area End ***** */}
-        <div className="container">
-          <div className="row">
-            <div className="col-12 d-flex justify-content-between align-items-center">
-              <h1 className="logo">My Website</h1>
-              {isOpen && <SignInModal onClose={() => setIsOpen(false)} />}
+      <div id="modal" className="popupContainer" style={{ display: "none" }}>
+        <div className="popupHeader">
+          <span className="header_title">Login</span>
+          <span className="modal_close">
+            <i className="fa fa-times" />
+          </span>
+        </div>
+        <section className="popupBody">
+          {/* Social Login */}
+          <div className="social_login">
+            <div className="">
+              <a href="#" className="social_box fb">
+                <span className="icon">
+                  <i className="fab fa-facebook" />
+                </span>
+                <span className="icon_title">Connect with Facebook</span>
+              </a>
+              <a href="#" className="social_box google">
+                <span className="icon">
+                  <i className="fab fa-google-plus" />
+                </span>
+                <span className="icon_title">Connect with Google</span>
+              </a>
+            </div>
+            <div className="centeredText">
+              <span>Or use your Email address</span>
+            </div>
+            <div className="action_btns">
+              <div className="one_half">
+                <a href="#" id="login_form" className="btn">
+                  Login
+                </a>
+              </div>
+              <div className="one_half last">
+                <a href="#" id="register_form" className="btn">
+                  Sign up
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+          {/* Username & Password Login form */}
+          <div className="user_login">
+            <form>
+              <label>Email / Username</label>
+              <input type="text" />
+              <br />
+              <label>Password</label>
+              <input type="password" />
+              <br />
+              <div className="checkbox">
+                <input id="remember" type="checkbox" />
+                <label htmlFor="remember">Remember me on this computer</label>
+              </div>
+              <div className="action_btns">
+                <div className="one_half">
+                  <a href="#" className="btn back_btn">
+                    <i className="fa fa-angle-double-left" /> Back
+                  </a>
+                </div>
+                <div className="one_half last">
+                  <a href="#" className="btn btn_red">
+                    Login
+                  </a>
+                </div>
+              </div>
+            </form>
+            <a href="#" className="forgot_password">
+              Forgot password?
+            </a>
+          </div>
+          {/* Register Form */}
+          <div className="user_register">
+            <form>
+              <label>Full Name</label>
+              <input type="text" />
+              <br />
+              <label>Email Address</label>
+              <input type="email" />
+              <br />
+              <label>Password</label>
+              <input type="password" />
+              <br />
+              <div className="checkbox">
+                <input id="send_updates" type="checkbox" />
+                <label htmlFor="send_updates">
+                  Send me occasional email updates
+                </label>
+              </div>
+              <div className="action_btns">
+                <div className="one_half">
+                  <a href="#" className="btn back_btn">
+                    <i className="fa fa-angle-double-left" /> Back
+                  </a>
+                </div>
+                <div className="one_half last">
+                  <a href="#" className="btn btn_red">
+                    Register
+                  </a>
+                </div>
+              </div>
+            </form>
+          </div>
+        </section>
+      </div>
       <div
         className="main-banner wow fadeIn"
         id="top"
