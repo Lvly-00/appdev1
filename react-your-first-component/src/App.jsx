e73ecc161a34810fbe43a1dd86afbff6e5d56ee0
Lovely <128210631+Lvly-00@users.noreply.github.com>
2025-11-09 20:08:50 +0800

    Done with defining a component

---
+++ react-your-first-component/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+export default function Profile() {
+  return (
+    <img
+      src="https://i.imgur.com/MK3eW3Am.jpg"
+      alt="Katherine Johnson"
+    />
+  )
+}
+
