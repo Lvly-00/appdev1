58c20d6f65101104bf0a42eb8c2a33c796628306
Lovely <128210631+Lvly-00@users.noreply.github.com>
2025-11-09 20:18:55 +0800

    Done with exporting and importing multiple components from the same file

--- react-importing-exporting-components/react-importing-exporting-components/src/App.jsx
+++ react-importing-exporting-components/react-importing-exporting-components/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-import Gallery from './Gallery.jsx';
-
+import Gallery from './Gallery';
+import { Profile } from './Gallery';
 
 export default function App() {
   return (
-    <Gallery />
+    <Profile />
   );
 }
 
