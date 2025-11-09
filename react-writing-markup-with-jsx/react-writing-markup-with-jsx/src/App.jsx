4b9fd3c73f25e91e1ce1e5ebe536de33bd2b5bfc
Lovely <128210631+Lvly-00@users.noreply.github.com>
2025-11-09 20:26:15 +0800

    Done with converting HTML to JSX

---
+++ react-writing-markup-with-jsx/react-writing-markup-with-jsx/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function TodoList() {
+  return (
+    <h1>Hedy Lamarr's Todos</h1>
+    <img 
+      src="https://i.imgur.com/yXOvdOSs.jpg" 
+      alt="Hedy Lamarr" 
+      class="photo"
+    >
+    <ul>
+      <li>Invent new traffic lights</li>
+      <li>Rehearse a movie scene</li>
+      <li>Improve the spectrum technology</li>
+    </ul>
+  );
+}
