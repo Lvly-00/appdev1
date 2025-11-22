a17dac107ac2ddf0d1fdd508d775f882881bd6a3
Lovely <128210631+Lvly-00@users.noreply.github.com>
2025-11-22 14:12:25 +0800

    Done with using double curlies: CSS and other objects in JSX

--- react-javascript-in-jsx-with-curly-braces/react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
 export default function TodoList() {
-  const name = 'Gregorio Y. Zara';
   return (
-    <h1>{name}'s To Do List</h1>
+    <ul style={{
+      backgroundColor: 'black',
+      color: 'red'
+    }}>
+      <li>Improve the videophone</li>
+      <li>Prepare aeronautics lectures</li>
+      <li>Work on the alcohol-fuelled engine</li>
+    </ul>
   );
 }
+
 
 
