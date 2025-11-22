cc726127c98023af9a1ebd721bed038c785dcc2f
Lovely <128210631+Lvly-00@users.noreply.github.com>
2025-11-22 14:14:00 +0800

    Done with more fun with JavaScript objects and curly braces

--- react-javascript-in-jsx-with-curly-braces/react-javascript-in-jsx-with-curly-braces/src/App.jsx
+++ react-javascript-in-jsx-with-curly-braces/react-javascript-in-jsx-with-curly-braces/src/App.jsx
@@ -1,17 +1,32 @@
 import React from 'react'
 
+const person = {
+  name: 'Gregorio Y. Zara',
+  theme: {
+    backgroundColor: 'beige',
+    color: 'maroon'
+  }
+};
+
 export default function TodoList() {
   return (
-    <ul style={{
-      backgroundColor: 'black',
-      color: 'red'
-    }}>
-      <li>Improve the videophone</li>
-      <li>Prepare aeronautics lectures</li>
-      <li>Work on the alcohol-fuelled engine</li>
-    </ul>
+    <div style={person.theme}>
+      <h1>{person.name}'s Todos</h1>
+      <img
+        className="avatar"
+        src="https://i.imgur.com/7vQD0fPs.jpg"
+        alt="Gregorio Y. Zara"
+      />
+      <ul>
+        <li>Improve the videophone</li>
+        <li>Prepare aeronautics lectures</li>
+        <li>Work on the alcohol-fuelled engine</li>
+      </ul>
+    </div>
   );
 }
+
+
 
 
 
